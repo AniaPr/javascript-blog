52182026e0d18e5932028f9406e55d0c19f968d7
Anna Prusińska <anna.prusinska12@gmail.com>
2023-01-10 21:54:03 +0100

    find the title element

--- js/script.js
+++ js/script.js
@@ -63,12 +63,15 @@ function generateTitleLinks() {
 
 	const articles = document.querySelectorAll(optArticleSelector);
 	for (let article of articles) {
-		/* get the article id */
+		/* [DONE] get the article id */
 
 		const articleId = article.getAttribute("id");
 		console.log(articleId);
 
 		/* find the title element */
+
+		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
 		/* get the title from the title element */
 		/* create HTML of the link */
 		/* insert link into titleList */
