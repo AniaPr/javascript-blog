310520fad41e3394364e0280ca8322fdcea06eaa
Anna Prusińska <anna.prusinska12@gmail.com>
2023-01-14 22:36:30 +0100

    add function generateTags and find all articles

--- js/script.js
+++ js/script.js
@@ -43,7 +43,8 @@ function titleClickHandler(event) {
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
   console.log('Function done');
@@ -66,7 +67,7 @@ function generateTitleLinks() {
     const articleId = article.getAttribute('id');
     console.log(articleId);
 
-    /* [DONE] find the title element get the title from the title element */
+    /* [DONE] find the title element and get the title from the title element */
 
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
@@ -97,3 +98,27 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+function generateTags() {
+  /* [DONE] find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+
+  /* START LOOP: for every article: */
+
+  for (let article of articles) {
+    /* find tags wrapper */
+    /* make html variable with empty string */
+    /* get tags from data-tags attribute */
+    /* split tags into array */
+    /* START LOOP: for each tag */
+    /* generate HTML of the link */
+    /* add generated code to html variable */
+    /* END LOOP: for each tag */
+    /* insert HTML of all the links into the tags wrapper */
+    /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
